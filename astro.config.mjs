568f48c689efab365ff01c00f542949023b6438b
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2026-02-10 08:12:33 +0200

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Noto Sans',
+        cssVariable: '--font-base',
+        formats: ['woff2', 'woff'],
+		weights: [400, 500, 600, 700],
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
