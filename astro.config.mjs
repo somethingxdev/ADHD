a5a3f1a21ecd2a60d1b3f13aeef57eb22127fe16
Ilya <hevnxv19@gmail.com>
2026-02-12 12:02:59 +0200

    style fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,34 @@ export default defineConfig({
   experimental: {
     fonts: [
       {
-        provider: fontProviders.google(),
-        name: 'Noto Sans',
+        provider: fontProviders.local(),
+        name: 'Segoe UI',
         cssVariable: '--font-base',
-        formats: ['woff2', 'woff'],
-		weights: [400, 500, 600, 700],
+        formats: ['woff2'],
+        options: {
+          variants: [
+            {
+              weight: 400,
+              style: 'normal',
+              src: ['./src/assets/fonts/SegoeUI.woff'],
+            },
+             {
+              weight: 500,
+              style: 'normal',
+              src: ['./src/assets/fonts/SegoeUI-Medium.ttf'],
+            },
+            {
+              weight: 600,
+              style: 'normal',
+              src: ['./src/assets/fonts/SegoeUI-SemiBold.woff'],
+            },
+            {
+              weight: 700,
+              style: 'normal',
+              src: ['./src/assets/fonts/SegoeUI-Bold.woff'],
+            },
+          ],
+        },
       },
     ],
   },
