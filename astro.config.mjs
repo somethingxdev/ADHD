4399d00d0cdf0c39d1c24e3f9ab710fae18cfc55
somethingxdev <143795997+somethingxdev@users.noreply.github.com>
2026-03-27 10:15:56 +0200

    new promo page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,40 +4,39 @@ import { defineConfig, fontProviders } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.local(),
-        name: 'Segoe UI',
-        cssVariable: '--font-base',
-        formats: ['woff2'],
-        options: {
-          variants: [
-            {
-              weight: 400,
-              style: 'normal',
-              src: ['./src/assets/fonts/SegoeUI.woff'],
-            },
-             {
-              weight: 500,
-              style: 'normal',
-              src: ['./src/assets/fonts/SegoeUI-Medium.ttf'],
-            },
-            {
-              weight: 600,
-              style: 'normal',
-              src: ['./src/assets/fonts/SegoeUI-SemiBold.woff'],
-            },
-            {
-              weight: 700,
-              style: 'normal',
-              src: ['./src/assets/fonts/SegoeUI-Bold.woff'],
-            },
-          ],
-        },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'Segoe UI',
+      cssVariable: '--font-base',
+      formats: ['woff2'],
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: ['./src/assets/fonts/SegoeUI.woff'],
+          },
+          {
+            weight: 500,
+            style: 'normal',
+            src: ['./src/assets/fonts/SegoeUI-Medium.ttf'],
+          },
+          {
+            weight: 600,
+            style: 'normal',
+            src: ['./src/assets/fonts/SegoeUI-SemiBold.woff'],
+          },
+          {
+            weight: 700,
+            style: 'normal',
+            src: ['./src/assets/fonts/SegoeUI-Bold.woff'],
+          },
+        ],
       },
-    ],
-  },
+    },
+  ],
+
   vite: {
     plugins: [tailwindcss(), yaml()],
   },
